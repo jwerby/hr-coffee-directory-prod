ac2cac282785448b3e2063da53d01515affdd3b1
jwerby <jwerby@users.noreply.github.com>
2024-12-03 10:45:13 -0500

    Add main page component

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { CoffeeDirectory } from '@/components/directory/CoffeeDirectory'
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <CoffeeDirectory />
+    </main>
+  )
+}
